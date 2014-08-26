29fdaed8bbb808a23c9452c23bdca6446c4c5ba6
Adam Chlipala <adamc@csail.mit.edu>
2014-08-26 17:44:33 -0400

    Harmonize quoting convention

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var monk = require('monk');
 var connection_string = 'localhost/mymongo';
 
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/mymongo';
 }
