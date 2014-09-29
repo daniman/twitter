f01490d313a9e3deb1fd6bf19e47dae4ab63ed56
Danielle Man <daniman0808@gmail.com>
2014-09-28 22:56:17 -0400

    right db name for openshift

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 
 var monk = require('monk');
 
-var connection_string = 'localhost/mymongo';
+var connection_string = 'localhost/twitter';
 
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/mymongo';
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/twitter';
 }
 
 var db = monk(connection_string);
